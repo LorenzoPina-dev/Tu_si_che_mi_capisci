0104105f041437e2b645dd1e12da2eed078cd44d
LorenzoPina-dev <lory.pina.03@gmail.com>
2022-03-21 20:48:15 +0100

    Continuo resetPassword

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,11 +1,12 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const fs = require('fs');
+const fs = require("fs");
 var multipart = require("connect-multiparty");
 var multipartMiddleware = multipart({ uploadDir: "uploads/" });
 var sha256 = require("js-sha256").sha256;
 var db = require("./../util/db");
 var nodemailer = require("nodemailer");
+let file = `./codiciReset/codiciReset${formatDate(new Date())}.txt`;
 var transporter = nodemailer.createTransport({
     service: "Gmail",
     auth: {
@@ -30,15 +31,16 @@ router.post("/login", (req, res) => {
         "SELECT * FROM utente WHERE Username = ? AND Password = ?", [req.query.username, req.query.password],
         (err, result) => {
             if (err) throw err;
-            console.log(result);
-            db.destroy();
-            res.json({ success: true, result: { Id: result[0].Id, ApiKey: result[0].ApiKey } });
+            res.json({
+                success: true,
+                result: { Id: result[0].Id, ApiKey: result[0].ApiKey },
+            });
         }
     );
 });
 
 router.post("/register", (req, res) => {
-    console.log("inizio")
+    console.log("inizio");
     let query = req.query;
     let d = new Date().getTime();
     key = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(c) {
@@ -54,30 +56,47 @@ router.post("/register", (req, res) => {
         "INSERT INTO utente (Username, Password, Email, Immagine, ApiKey) VALUES (?,?,?,?,?)", [query.username, criptata, query.email, query.immagine, key],
         (err, result) => {
             if (err) throw err;
-            console.log(result);
-            res.json({
-                success: true,
-            });
+            res.json({ success: true, result: "" });
         }
     );
 });
 router.get("/resetPassword", (req, res) => {
     let codice = "";
-    let codici = fs.readFileSync(`codiciReset${formatDate(new Date())}.txt`, "utf8").split("\r\n");
+    let mail = req.query.email;
+    const emailRegexp = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    if (!emailRegexp.test(mail)) {
+        res.json({ success: false, error: "mail errata" });
+        return;
+    }
+    let codici = [];
+    if (fs.existsSync(file))
+        codici = fs.readFileSync(file, "utf8").split("\r\n");
     do {
         for (let i = 0; i < 7; i++) codice += getRndInteger(0, 10).toString();
-    } while (codici.includes(codice))
-    codici.push(codice);
-    fs.appendFileSync(`codiciReset${formatDate(new Date())}.txt`, "\r\n" + codice);
-    //sendMail(res,req.query.email,req.query.email,"codice reset password","il codice per il reset è: " + codice);
+    } while (codici.includes(JSON.stringify({ mail: mail, codice: codice })));
+    if (!fs.existsSync(file))
+        fs.appendFileSync(file, JSON.stringify({ mail: mail, codice: codice }));
+    else
+        fs.appendFileSync(file, "\r\n" + JSON.stringify({ mail: mail, codice: codice }));
+
+    sendMail(res, mail, mail, "codice reset password", "il codice per il reset è: " + codice);
     res.json({ success: true, message: "invio codice" });
-})
-router.put("/cambiaPassword/:codice", (req, res) => {
+});
+router.get("/cambiaPassword", (req, res) => {
+    let query = req.query;
+    let codici = fs.readFileSync(file, "utf8").split("\r\n").map(val => JSON.parse(val));
+    let mail = "";
+    for (c of codici)
+        if (c.codice == query.codice)
+            mail = c.mail;
+    if (query.nuovaPassword != query.confermanuovaPassword) {
+        res.json({ success: false, error: "le password non coincidono" });
+        return;
+    }
     db.query(
-        "INSERT INTO utente (Username, Password, Email, Immagine, ApiKey) VALUES (?,?,?,?,?)", [query.username, criptata, query.email, query.immagine, key],
+        "UPDATE utente SET Password=? where Email=?", [sha256.hex(query.nuovaPassword), mail],
         (err, result) => {
             if (err) throw err;
-            console.log(result);
             res.json({
                 success: true,
             });
@@ -103,7 +122,9 @@ const sendMail = (res, username, mailTo, subject, body) => {
                 console.log("Messaggio inviato: " + info.response);
             }
         });
-    } catch (e) { console.log(e) }
+    } catch (e) {
+        console.log(e);
+    }
 };
 
 function padTo2Digits(num) {
