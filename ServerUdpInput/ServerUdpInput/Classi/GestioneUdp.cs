41a14e1c8b460665e28a8d7d8cf10f897d29f370
LorenzoPina-dev <lory.pina.03@gmail.com>
2022-03-26 18:17:03 +0100

    Creazione server udp che riceve le informazioni dalla dock e le salva su file

---
+++ ServerUdpInput/ServerUdpInput/Classi/GestioneUdp.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerUdpInput.Classi
+{
+    class GestioneUdp
+    {
+        private UdpClient client;
+        private static GestioneUdp instance = null;
+
+        private GestioneUdp()
+        {
+            client = new UdpClient(12345);
+            client.Client.ReceiveTimeout = 5000;
+        }
+        public static GestioneUdp Instance()
+        {
+            if (instance == null)
+                instance = new GestioneUdp();
+            return instance;
+        }
+        public  void Invia(Messaggio m)
+        {
+            byte[] send = Encoding.ASCII.GetBytes(m.Testo);
+            client.Send(send, send.Length,m.Ip,m.porta);
+        }
+        public  Messaggio Ricevi()
+        {
+            IPEndPoint porta = new IPEndPoint(IPAddress.Loopback, 0);
+            byte[] ricevuto = client.Receive(ref porta);
+            return new Messaggio(Encoding.ASCII.GetString(ricevuto));
+        }
+    }
+}
--- ServerUdpInput/ServerUdpInput/Program.cs
+++ ServerUdpInput/ServerUdpInput/Program.cs
@@ -4,23 +4,62 @@ using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
-
+using ServerUdpInput.Classi;
 namespace ServerUdpInput
 {
     class Program
     {
-        public Queue<Messaggio> in,out;
+        public static bool termina = false;
         static void Main(string[] args)
         {
-            UdpClient client = new UdpClient(12345);
-            IPEndPoint endP = new IPEndPoint(IPAddress.Any, 0);
-            while(true)
+            new Thread(Ricevi).Start();
+            new Thread(Invia).Start();
+            new Thread(Elabora).Start();
+            new Thread(Elabora).Start();
+            while (!termina)
             {
-                string ris=Encoding.ASCII.GetString(client.Receive(ref endP));
-
+                Console.WriteLine("Inserisci exit per terminare il programma");
+                termina = Console.ReadLine().ToUpper() == "exit".ToUpper();
             }
         }
-        public
+        public static void Ricevi()
+        {
+            while(!termina)
+            {
+                try
+                {
+                    DatiCondivisi.Instance().AddMessaggioRicevuti(GestioneUdp.Instance().Ricevi());
+                }
+                catch (Exception) { }
+            }
+        }
+        public static void Invia()
+        {
+            while (!termina)
+            {
+                try
+                {
+                    Messaggio daInviare = DatiCondivisi.Instance().GetMessaggioDaInviare();
+                    if(daInviare!=null)
+                    GestioneUdp.Instance().Invia(daInviare);
+                }
+                catch (Exception) { }
+            }
+        }
+        public static void Elabora()
+        {
+            while (!termina)
+            {
+                try
+                {
+                    Messaggio daElaborare = DatiCondivisi.Instance().GetMessaggioRicevuti();
+                    if(daElaborare!=null)
+                        DatiCondivisi.Instance().AddMessaggioDaInviare(DatiCondivisi.Instance().Elabora(daElaborare));
+                }
+                catch (Exception) { }
+            }
+        }
     }
 }
