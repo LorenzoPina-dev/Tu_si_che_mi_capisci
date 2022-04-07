3c2beba0e7fe5e0f342f4222e3323b17420b5b97
giorgia-l <82999381+giorgia-l@users.noreply.github.com>
2022-04-07 09:59:03 +0200

    update code

--- AI/index.py
+++ AI/index.py
@@ -2,3 +2,15 @@ import numpy as np
 import cv2
 
 from simple_facerec import SimpleFacerec
+
+#Load camera
+cap = cv2.VideoCapture(0)
+
+
+while True:
+    ret, frame = cap.read()
+
+    cv2.imshow("Frame", frame)
+
+    key = cv2.waitKey(1)
+    
