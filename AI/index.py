d56a11cc4e9f7cbc5666ea5c47901ba44234bbc1
LorenzoPina-dev <lory.pina.03@gmail.com>
2022-04-16 15:56:37 +0200

    integrazione ia server remoto

--- AI/index.py
+++ AI/index.py
@@ -1,38 +1,55 @@
 import cv2
-
+import os
+import glob
+import json
 from simple_facerec import SimpleFacerec
+import face_recognition
+import base64
+from imageio import imread
+import io
 
-#le immagini che vengono salvate devono chiamarsi nomedellapersona.ext
+import requests
+import json
 
-#funzione per importare tutti i volti che sono nella cartella
+
+keyk="a2444840-cb9a-479d-bd3f-a4fa4a2f23f8"
+
+
+def GetDispositivi():
+    x = requests.get('http://80.22.36.186/'+keyk+'/dispositivi?tipo=1');
+    y = json.loads(x.text)
+    arr= y['result']['dispositivo']
+    return arr;
+
+
+def inviaVoltoTrovato(name, img,idDispositivo,face_encodings):
+    files = {'immagine': (img, open(img, 'rb'), 'image/jpg', {'Expires': '0'})}
+    requests.post('http://80.22.36.186/'+keyk+'/voltoTrovato/add', data={'idDispositivo': idDispositivo, 'nome': name,'vettVolto':face_encodings}, files=files)
+
 sfr = SimpleFacerec()
-sfr.load_encoding_images("images/") #nome della cartella dove sono le foto
-
-
-
-#Load camera
-cap = cv2.VideoCapture(0) #indice della camera, (se più camere)
-
-
-#otteniamo lo stream in real time
+sfr.load_encoding_images()
 while True:
-    ret, frame = cap.read()
+    images_path = glob.glob(os.path.join("Volto/", "*.*"))
 
-    face_locationes, face_names =sfr.detect_known_faces(frame)
-    for face_loc, name in zip(face_locationes, face_names):
-        #print(face_loc) #la posizione del volto nella cam
-        y1, x1, y2, x2 = face_loc[0], face_loc[1], face_loc[2], face_loc[3]
+    i=0
+    # Store image encoding and names
+    for img_path in images_path:
+        try:
+            f=open(img_path)
+            inp=json.loads(f.readlines()[0])
+            cv2_img = cv2.cvtColor(imread(io.BytesIO(base64.b64decode(inp["Dati"][0]))), cv2.COLOR_RGB2BGR)
+            print("immagine decodificata "+img_path)
+            f.close()
+            face_locations = face_recognition.face_locations(cv2_img)
+            face_encodings = face_recognition.face_encodings(cv2_img, face_locations)[0]
+            name =sfr.detect_known_faces(face_encodings)
+            print(img_path)
+            cv2.imwrite("temp.jpg", cv2_img)
+            inviaVoltoTrovato(name,"temp.jpg",inp["IdDispositivo"],face_encodings)
+            os.remove(img_path)
+        except:
+            print("err")
+        i+=1
+        key = cv2.waitKey(1) #ogni quanti millisecondi andare al frame successivo
 
-        cv2.putText(frame, name, (x1,y1 - 10), cv2.FONT_HERSHEY_DUPLEX, 1, (0,0,0), 2)
-        cv2.rectangle(frame, (x1,y1), (x2,y2), (0,0,255), 4)
 
-
-    cv2.imshow("Frame", frame)
-
-    key = cv2.waitKey(1) #ogni quanti millisecondi andare al frame successivo
-
-cap.release()
-cv2.destroyAllWindows() #mostra in una finestra cosa vede la cam
-
-
-    
--- AI/simple_facerec.py
+++ AI/simple_facerec.py
@@ -3,6 +3,27 @@ import cv2
 import os
 import glob
 import numpy as np
+from urllib.request import urlopen
+import requests
+import json
+
+keyk="a2444840-cb9a-479d-bd3f-a4fa4a2f23f8"
+
+
+def GetVoltiRegistrati():
+    x = requests.get('http://80.22.36.186/'+keyk+'/voltoRegistrato');
+    y = json.loads(x.text)
+    arr= y['result']['voltiRegistrati']
+    return arr
+
+def SetEncodingVolto(record,punti):
+    x=requests.put('http://80.22.36.186/'+keyk+'/voltoRegistrato/addPunti', data={'id': record["Id"], 'punti': str(punti.tolist())})
+
+def GetImmagine(basename):
+    req = urlopen('http://80.22.36.186/'+keyk+'/immagine/voltoregistrato?nomefile='+basename)
+    arr = np.asarray(bytearray(req.read()), dtype=np.uint8)
+    img = cv2.imdecode(arr, -1) # 'Load it as it is'
+    return img
 
 class SimpleFacerec:
     def __init__(self):
@@ -12,60 +33,28 @@ class SimpleFacerec:
         # Resize frame for a faster speed
         self.frame_resizing = 0.25
 
-    def load_encoding_images(self, images_path):
-        """
-        Load encoding images from path
-        :param images_path:
-        :return:
-        """
-        # Load Images
-        images_path = glob.glob(os.path.join(images_path, "*.*"))
-
-        print("{} encoding images found.".format(len(images_path)))
-
-        # Store image encoding and names
-        for img_path in images_path:
-            img = cv2.imread(img_path)
-            rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-            # Get the filename only from the initial file path.
-            basename = os.path.basename(img_path)
-            (filename, ext) = os.path.splitext(basename)
-            # Get encoding
-            img_encoding = face_recognition.face_encodings(rgb_img)[0]
-
-            # Store file name and file encoding
-            self.known_face_encodings.append(img_encoding)
-            self.known_face_names.append(filename)
+    def load_encoding_images(self):
+        VoltiRegistrati=GetVoltiRegistrati()
+        i=0
+        for volto in VoltiRegistrati:
+            try:
+                img=GetImmagine(volto["Immagine"])
+                if(volto["VettoreVolto"]!=null):
+                    img_encoding=volto["VettoreVolto"]
+                else:
+                    img_encoding = face_recognition.face_encodings(img)[0]
+                    SetEncodingVolto(volto,img_encoding)
+                self.known_face_encodings.append(img_encoding)
+                self.known_face_names.append(volto["Nome"])
+            except:
+                print("errore")
         print("Encoding images loaded")
 
-    def detect_known_faces(self, frame):
-        small_frame = cv2.resize(frame, (0, 0), fx=self.frame_resizing, fy=self.frame_resizing)
-        # Find all the faces and face encodings in the current frame of video
-        # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-        rgb_small_frame = cv2.cvtColor(small_frame, cv2.COLOR_BGR2RGB)
-        face_locations = face_recognition.face_locations(rgb_small_frame)
-        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
-
-        face_names = []
-        for face_encoding in face_encodings:
-            # See if the face is a match for the known face(s)
-            matches = face_recognition.compare_faces(self.known_face_encodings, face_encoding)
-            name = "Unknown"
-
-            # # If a match was found in known_face_encodings, just use the first one.
-            # if True in matches:
-            #     first_match_index = matches.index(True)
-            #     name = known_face_names[first_match_index]
-
-            # Or instead, use the known face with the smallest distance to the new face
-            face_distances = face_recognition.face_distance(self.known_face_encodings, face_encoding)
-            best_match_index = np.argmin(face_distances)
-            if matches[best_match_index]:
-                name = self.known_face_names[best_match_index]
-            face_names.append(name)
-
-        # Convert to numpy array to adjust coordinates with frame resizing quickly
-        face_locations = np.array(face_locations)
-        face_locations = face_locations / self.frame_resizing
-        return face_locations.astype(int), face_names
+    def detect_known_faces(self, face_encoding):
+        matches = face_recognition.compare_faces(self.known_face_encodings, face_encoding)
+        face_names = "Unknown"
+        face_distances = face_recognition.face_distance(self.known_face_encodings, face_encoding)
+        best_match_index = np.argmin(face_distances)
+        if matches[best_match_index]:
+            face_names = self.known_face_names[best_match_index]
+        return face_names
