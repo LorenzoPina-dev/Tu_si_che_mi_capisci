1613fa564e5969ccc18f4421b549cf595e5ca1a9
LorenzoPina-dev <lory.pina.03@gmail.com>
2022-04-18 17:49:51 +0200

    getDispositivi che vede la dock

--- DockLocale/index.py
+++ DockLocale/index.py
@@ -1,16 +1,19 @@
+
+from __future__ import print_function
+from pygrabber.dshow_graph import FilterGraph
 from multiprocessing import Process, Pipe
 from video import GestioneCam
+import speech_recognition as s_r
 import json
 import socket
-
-
+import os
+import cv2
 bufferSize  = 1024
 def f(conn):
     print(conn.recv())   # prints "[42, None, 'hello']"
     conn.close()
 
 if __name__ == '__main__':
-
     localIP = "127.0.0.1"
 
     localPort   = 12345
@@ -23,6 +26,14 @@ if __name__ == '__main__':
     while(True):
         bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
         message = bytesAddressPair[0]
-        parent_conn.send(json.loads(message)["key"])
+        message=message.decode("utf-8")
+        print(message)
+        if message=="getDispositivi/video":
+                graph = FilterGraph()
+                UDPServerSocket.sendto(str.encode(str(json.dumps({'dispositivi':graph.get_input_devices()}))),bytesAddressPair[1])
+        elif message=="getDispositivi/audio":
+            UDPServerSocket.sendto(str.encode(str(json.dumps({'dispositivi':s_r.Microphone.list_microphone_names()}))),bytesAddressPair[1])
+        else:
+            parent_conn.send(message)
     p.join()
     parent_conn.close()
